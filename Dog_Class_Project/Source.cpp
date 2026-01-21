d2144eaa74e7dc1d314df3c9d07bcb6c51ae5630
oscar <oscar@ONPLANK>
2026-01-20 18:54:28 -0600

    Add project files.

---
+++ Dog_Class_Project/Source.cpp
@@ -0,0 +1,68 @@
+// Oscar Navarrete
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Dog
+{
+private:
+	string name;
+	int age;
+	string breed;
+public:
+	void setname(string dogName)
+	{
+		name = dogName;
+	}
+
+	void setage(int dogAge)
+	{
+		age = dogAge;
+	}
+
+	void setbreed(string dogBreed)
+	{
+		breed = dogBreed;
+	}
+
+	string getname()
+	{
+		return name;
+	}
+	int getage()
+	{
+		return age;
+	}
+	string getbreed()
+	{
+		return breed;
+	}
+	void bark()
+	{
+		cout << name << " Woof Woof!" << endl;
+	}
+
+	void displayData()
+	{
+		cout << "Name: " << name << endl;
+		cout << "Age: " << age << endl;
+		cout << "Breed: " << breed << endl;
+	}
+};
+
+int main()
+{
+	Dog myDog;
+
+
+	myDog.setname("Buddy");
+	myDog.setage(5);
+	myDog.setbreed("Golden Retriever");
+	myDog.displayData();
+	myDog.bark();
+
+
+	return 0;
+}
